519095f86897cb508ed6a58a3c2e4cbd6ccd70e0
ymr-cyh <83774342+ymr-cyh@users.noreply.github.com>
2022-03-20 20:12:03 +0800

    Delete javaLesson directory

--- javaLesson/Exa3_7.java
+++
@@ -1,7 +0,0 @@
-package javaLesson;
-
-public class Exa3_7 {
-    public static void main(String[] args) {
-        
-    }
-}
--- javaLesson/Exe2_4_1.java
+++
@@ -1,9 +0,0 @@
-package javaLesson;
-
-public class Exe2_4_1 {
-    public static void main(String[] args) {
-        for (int i = 20302; i <= 20322; i++) {
-            System.out.println((char) i);
-        }
-    }
-}
