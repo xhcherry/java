036cf50fca4c0e171901421868d0989f5e05c3f8
Cherry <83774342+xhcherry@users.noreply.github.com>
2022-05-25 21:19:10 +0800

    Add files via upload

--- Experiment/Exp8/TestGame.java
+++ Experiment/Exp8/TestGame.java
@@ -2,8 +2,8 @@ package Experiment.Exp8;
 
 public class TestGame {
     public static void main(String[] args) {
-//        NumberGame game = new NumberGame(33);
-//        game.startGame();
+        NumberGame game = new NumberGame(0);
+        game.startGame();
     }
 }
 
