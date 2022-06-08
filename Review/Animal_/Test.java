e0d5ebea1bfd97fa19a73b403173d85f95eaf3f9
Cherry <83774342+xhcherry@users.noreply.github.com>
2022-06-08 15:30:49 +0800

    Add files via upload

---
+++ Review/Animal_/Test.java
@@ -0,0 +1,52 @@
+package Review.Animal_;
+
+interface Protected {
+    void protect();
+}
+
+class Animal implements Protected {
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void getName() {
+    }
+
+    public void protect() {
+    }
+}
+
+class Tiger extends Animal {
+    public Tiger(String name) {
+        super(name);
+    }
+
+    public void protect() {
+        System.out.println(name);
+    }
+}
+
+class Monkey extends Animal {
+    public Monkey(String name) {
+        super(name);
+    }
+
+    public void protect() {
+        System.out.println(name);
+    }
+}
+
+public class Test {
+    public static void main(String[] args) {
+        Protected T = new Tiger("hu");
+        T.protect();
+        T = new Monkey("hou");
+        T.protect();
+    }
+}
