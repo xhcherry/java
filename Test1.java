e0ce58f3357c3a5cdf0e1f30c727b42740b7c6bd
xhaocherry <cyhcherry@outlook.com>
2022-11-28 22:10:52 +0800

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,11 +1,12 @@
 import java.sql.*;
+
 public class Test1 {
     public static void main(String[] args) throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://localhost/mysql?user=root&password=123456";
         Connection conn = DriverManager.getConnection(url);
         DatabaseMetaData dbmd = conn.getMetaData();
-        System.out.print("JDBC驱动程序："+dbmd.getDriverName()+","+dbmd.getDriverVersion()+"\nJDBC URL:"+dbmd.getURL()+"\n数据库:"+dbmd.getDatabaseProductName()+",版本:"+dbmd.getDatabaseProductVersion()+"，用户名:"+dbmd.getUserName());
+        System.out.print("JDBC驱动程序：" + dbmd.getDriverName() + "," + dbmd.getDriverVersion() + "\nJDBC URL:" + dbmd.getURL() + "\n数据库:" + dbmd.getDatabaseProductName() + ",版本:" + dbmd.getDatabaseProductVersion() + "，用户名:" + dbmd.getUserName());
         conn.close();
     }
 }
